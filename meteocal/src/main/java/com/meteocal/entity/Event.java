9f7fba237cfb05d340d1da2688ec82ff153dbcd0
francesco.fermi <francesco.fermi@gmail.com>
2014-12-15 10:17:41 +0000

    auto-generate entities

--- meteocal/src/main/java/com/meteocal/entity/Event.java
+++ meteocal/src/main/java/com/meteocal/entity/Event.java
@@ -6,34 +6,208 @@
 package com.meteocal.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author Manuel
+ * @author Francesco
  */
 @Entity
+@Table(name = "events")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Event.findAll", query = "SELECT e FROM Event e"),
+    @NamedQuery(name = "Event.findByEventId", query = "SELECT e FROM Event e WHERE e.eventId = :eventId"),
+    @NamedQuery(name = "Event.findByCreator", query = "SELECT e FROM Event e WHERE e.creator = :creator"),
+    @NamedQuery(name = "Event.findByName", query = "SELECT e FROM Event e WHERE e.name = :name"),
+    @NamedQuery(name = "Event.findByLocation", query = "SELECT e FROM Event e WHERE e.location = :location"),
+    @NamedQuery(name = "Event.findByStart", query = "SELECT e FROM Event e WHERE e.start = :start"),
+    @NamedQuery(name = "Event.findByEnd", query = "SELECT e FROM Event e WHERE e.end = :end"),
+    @NamedQuery(name = "Event.findByPublic1", query = "SELECT e FROM Event e WHERE e.public1 = :public1"),
+    @NamedQuery(name = "Event.findByDescription", query = "SELECT e FROM Event e WHERE e.description = :description"),
+    @NamedQuery(name = "Event.findByPersonal", query = "SELECT e FROM Event e WHERE e.personal = :personal")
+})
 public class Event implements Serializable {
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
+    private Collection<Answer> answerCollection;
+    
+    @OneToMany(mappedBy = "eventId")
+    private Collection<Information> informationCollection;
+    
+    @JoinTable(name = "invitations", joinColumns = {
+        @JoinColumn(name = "event_id", referencedColumnName = "event_id")}, inverseJoinColumns = {
+        @JoinColumn(name = "user_id", referencedColumnName = "user_id")})
+    @ManyToMany
+    private Collection<User> userCollection;
+    
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "event")
+    private Weather weather;
+    
     private static final long serialVersionUID = 1L;
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "event_id")
+    private Integer eventId;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "creator")
+    private int creator;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "name")
+    private String name;
+   
+    @Size(max = 100)
+    @Column(name = "location")
+    private String location;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "start")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date start;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "end")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date end;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "public")
+    private boolean public1;
+    
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "personal")
+    private boolean personal;
 
-    public Long getId() {
-        return id;
+    public Event() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Event(Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    public Event(Integer eventId, int creator, String name, Date start, Date end, boolean public1, boolean personal) {
+        this.eventId = eventId;
+        this.creator = creator;
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.public1 = public1;
+        this.personal = personal;
+    }
+
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getCreator() {
+        return creator;
+    }
+
+    public void setCreator(int creator) {
+        this.creator = creator;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public boolean getPublic1() {
+        return public1;
+    }
+
+    public void setPublic1(boolean public1) {
+        this.public1 = public1;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(boolean personal) {
+        this.personal = personal;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (eventId != null ? eventId.hashCode() : 0);
         return hash;
     }
 
@@ -44,7 +218,7 @@ public class Event implements Serializable {
             return false;
         }
         Event other = (Event) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.eventId == null && other.eventId != null) || (this.eventId != null && !this.eventId.equals(other.eventId))) {
             return false;
         }
         return true;
@@ -52,7 +226,42 @@ public class Event implements Serializable {
 
     @Override
     public String toString() {
-        return "com.meteocal.entity.Event[ id=" + id + " ]";
+        return "com.meteocal.entity.Event[ eventId=" + eventId + " ]";
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+    }
+
+    @XmlTransient
+    public Collection<User> getUserCollection() {
+        return userCollection;
+    }
+
+    public void setUserCollection(Collection<User> userCollection) {
+        this.userCollection = userCollection;
+    }
+
+    @XmlTransient
+    public Collection<Information> getInformationCollection() {
+        return informationCollection;
+    }
+
+    public void setInformationCollection(Collection<Information> informationCollection) {
+        this.informationCollection = informationCollection;
+    }
+
+    @XmlTransient
+    public Collection<Answer> getAnswerCollection() {
+        return answerCollection;
+    }
+
+    public void setAnswerCollection(Collection<Answer> answerCollection) {
+        this.answerCollection = answerCollection;
     }
     
 }
