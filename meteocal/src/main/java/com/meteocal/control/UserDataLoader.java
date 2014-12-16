28b303617d0e95e23291d154e076e0e3c68e429b
manuel.grillo@aol.it <manuel.grillo@aol.it@59154255-1539-2a9e-40eb-080a1e93b571>
2014-12-16 16:30:22 +0000

    Partial structure #2

---
+++ meteocal/src/main/java/com/meteocal/control/UserDataLoader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.meteocal.control;
+
+import com.meteocal.entity.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Manuel
+ */
+public class UserDataLoader {
+    
+    //TODO - It has to be changed accordingly to our needs.
+    /**
+     * It provides the required information to compose the personal page of the user.
+     * 
+     * @param u user
+     * @return
+     */
+    public ArrayList<Object> loadUserProfile(User u){
+        return null;
+    }
+    
+    
+    
+}
