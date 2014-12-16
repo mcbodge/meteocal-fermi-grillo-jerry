f3e5759bd59b2a5b22fca53368881ade01a392e2
manuel.grillo <manuel.grillo@aol.it>
2014-12-16 16:30:22 +0000

    Partial structure #2

---
+++ meteocal/src/main/java/com/meteocal/control/HomePageLoader.java
@@ -0,0 +1,31 @@
+package com.meteocal.control;
+
+/**
+ * FROM/TO - B:HomePage
+ * TO - B:RegistrationPage, B:ProblemsPage
+ * 
+ * @author Manuel
+ */
+public class HomePageLoader {
+    
+    //TODO
+    /**
+     * Returns the URL of the registration page (registration.xhtml). It refers to the RegistrationPage boundary class.
+     * 
+     * @return the URL of the registration page.
+     */
+    public String loadRegistrationPage(){
+        return null;
+    }
+    
+    //TODO
+    /**
+     * Returns the URL of the problems page (problems.xhtml). It refers to the ProblemsPage boundary class.
+     * 
+     * @return the URL of the problems page.
+     */
+    public String homePageLoader(){
+        return null;
+    }
+    
+}
