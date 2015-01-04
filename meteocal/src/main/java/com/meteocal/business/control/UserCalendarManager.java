164d8051d1774dc3c2e2ff29af6e557d1476aeab
francesco.fermi@gmail.com <francesco.fermi@gmail.com@59154255-1539-2a9e-40eb-080a1e93b571>
2015-01-04 19:05:52 +0000

    UserCalendarMnanager ->RC , EventCreationManager now is a Singleton

--- meteocal/src/main/java/com/meteocal/business/control/UserCalendarManager.java
+++ meteocal/src/main/java/com/meteocal/business/control/UserCalendarManager.java
@@ -10,6 +10,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.context.FacesContext;
@@ -30,10 +36,7 @@ public class UserCalendarManager {
     
     private final String EXT = ".bin";
     
-    //TODO add event infos
-    //nb we need to put also the information about the owner of the calendar 
-    //(if - for some reason - is tricky, just put its userId as the name of the file).
-    // aaa.bin
+    //TODO RC
     /**
      * It start the download of the file by putting it in a temporary folder and redirecting the browser to that path.
      * 
@@ -58,8 +61,8 @@ public class UserCalendarManager {
             //get all personal public events
             for (Event e : u.getEventCreatedCollection()){
                 if(e.isPersonal() && e.isPublicEvent()){
-                   //write in file
-                   writer.write("\n"+e.getEventId()+"\t"+e.getName());
+                   //write in file  eventid   name    location    start   end   descr   constraint
+                   writer.write("\n"+e.getEventId().toString() + "\t" + e.getName() + "\t" + e.getLocation() + "\t" + e.getStart().toString() + "\t" + e.getEnd().toString() + "\t" + e.getDescription() + "\t" + e.getWeather().getConstraint().toString());
                 }
             }   
             writer.close();
@@ -89,44 +92,24 @@ public class UserCalendarManager {
     /**
      * It verify the consistency of the uploaded file:
      * - verify the correct type/extension;
-     * - verify the correct db structure.
+     * - verify the correct db structure. ?! <-- deprecated
      * 
      * @param f the result of the verification (true if the file can be correctly imported)
      * @return 
      */
     private boolean verifyFile(File f){
         double kilobytes = (f.length() / 1024);
-        String line;
-        InputStream is;
-        BufferedReader reader;
-        String user_name;
-        //size < 2MB
-        if(kilobytes < 2048){
-            //check extension
-            if(f.getName().substring(f.getName().length()-4).equals(EXT)){
-                try {
-                    is = new FileInputStream(f);
-                    reader = new BufferedReader(new InputStreamReader(is));
-                    //check user existance.
-                    user_name = reader.readLine();
-                    
-                    //Done with the file
-                    reader.close();
-                    //reader = null;
-                    //is = null;
-                    return true;
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(UserCalendarManager.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException exIO) {
-                    Logger.getLogger(UserCalendarManager.class.getName()).log(Level.SEVERE, null, exIO);
-                }
-            }
+                
+        //check size < 2MB
+        if(kilobytes < 2048){            
+            if(f.getName().substring(f.getName().length()-4).equals(EXT))
+                return true;           
         }
         return false;
     }
     
     
-    //TODO 
+    //TODO RC
     /**
      * It extracts the interesting events from the calendar:
      * If the user who is importing the calendar is the same who has exported it:
@@ -138,35 +121,73 @@ public class UserCalendarManager {
      * @param f the previously imported and verified file.
      * @param u the user who is importing the file.
      */
-    private void extractFromFile(File f, User u){
-        String username;
+    private void extractFromFile(File f, User u) {
+        
         InputStream is;
         BufferedReader reader;
         String line;
+        String user_file, e_id, e_name, e_str_location, e_str_start, e_str_end, e_descr, e_constraint;
+        StringTokenizer token;
+        
         try {
+            //open stream
             is = new FileInputStream(f);
             reader = new BufferedReader(new InputStreamReader(is));
+            
             //first line = user_id
-            username = reader.readLine();
-            EventCreationManager ecm = new EventCreationManager();
+            user_file = reader.readLine();
             
             while ((line = reader.readLine()) != null){
-                //stringtokenizer per info evento
-                String eventName;
-                if (username.equals(u.getUserName())){
-                    eventName = e.getName() + " [Imported]";
-                } else {
-                    eventName = e.getName() +" ["+ username +"]";
-                }
-                ecm.newEvent(u, eventName, e.getStart(), e.getEnd(), e.getLocation(), null, true, null, e.getDescription());
+                //line = eventid   name    location    start   end   descr   constraint
+                token = new StringTokenizer(line, "\t");
                 
+                e_id = token.nextToken();
+                e_name = token.nextToken();
+                e_str_location = token.nextToken(); 
+                e_str_start = token.nextToken();
+                e_str_end = token.nextToken();
+                e_descr = token.nextToken();
+                e_constraint = token.nextToken();
+                
+                //<editor-fold defaultstate="state" desc="JDoc Date.toString()">
+                /**
+                 * Converts this Date object to a String of the form:
+                 *              dow mon dd hh:mm:ss zzz yyyy
+                 * where:
+                 * dow is the day of the week (Sun, Mon, Tue, Wed, Thu, Fri, Sat).
+                 * mon is the month (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec).
+                 * dd is the day of the month (01 through 31), as two decimal digits.
+                 * hh is the hour of the day (00 through 23), as two decimal digits.
+                 * mm is the minute within the hour (00 through 59), as two decimal digits.
+                 * ss is the second within the minute (00 through 61, as two decimal digits.
+                 * zzz is the time zone (and may reflect daylight saving time). Standard time zone abbreviations include those recognized by the method parse. If time zone information is not available, then zzz is empty - that is, it consists of no characters at all.
+                 * yyyy is the year, as four decimal digits.
+                 * 
+                 */           
+                //</editor-fold>               
+                DateFormat df = new SimpleDateFormat("dow mon dd hh:mm:ss zzz yyyy");
+                Date e_start =  df.parse(e_str_start);
+                Date e_end =  df.parse(e_str_end);
+                
+                if (user_file.equals(u.getUserName())){
+                    e_name = e_name + " [Imported]";
+                } else {
+                    e_name = e_name +" ["+ user_file +"]";
+                }
+                EventCreationManager.getInstance().newEvent(u, e_name, e_start, e_end, e_str_location, null, true, Integer.parseInt(e_constraint), e_descr);
             }
-            //ecm = null;
+            
+            //close stream
+            reader.close();
+            is.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(UserCalendarManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
+        } catch (IOException | ParseException ex) {
             Logger.getLogger(UserCalendarManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
+       
+        
+        
     }
     
 
