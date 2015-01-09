3798c28613b32517d11a1816236e95f4fb4c4e99
francesco.fermi <francesco.fermi@gmail.com>
2015-01-09 10:46:53 +0000

    personal

--- meteocal/src/main/java/com/meteocal/business/boundary/RegistrationFacade.java
+++ meteocal/src/main/java/com/meteocal/business/boundary/RegistrationFacade.java
@@ -6,10 +6,13 @@
 package com.meteocal.business.boundary;
 
 import com.meteocal.business.control.ProfileDataManager;
+import com.meteocal.business.entity.Group;
+import com.meteocal.business.entity.User;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -20,22 +23,60 @@ public class RegistrationFacade {
 
     @PersistenceContext
     EntityManager em;
-    
+
     @Inject
     ProfileDataManager pdm;
-    
+
     //TODO check user
     /**
      * write in db
+     *
      * @param firstname
      * @param lastname
      * @param username
      * @param email
      * @param password
-     * 
+     *
      * @return true if registration is ok.
      */
-    public boolean registerUser(String firstname, String lastname, String username, String email, String password){
-        return pdm.newUser(firstname, lastname, username, email, password);
-    } 
+    public boolean registerUser(String firstname, String lastname, String username, String email, String password) {
+        User user = null;        
+        if (verifySubmittedData(firstname, lastname, username, email, password)) {
+            user = pdm.newUser(firstname, lastname, username, email, password);
+            user.setGroupName(Group.USERS);
+            user = em.merge(user);
+            em.flush();
+        }
+        return user != null;
+    }
+
+    /**
+     *
+     * @param first first name
+     * @param last last name
+     * @param username user name
+     * @param email email
+     * @param password password
+     * @return true = valid data
+     */
+    private boolean verifySubmittedData(String first, String last, String username, String email, String password) {
+
+        if (first != null && !first.equals("") && last != null && !last.equals("") && username != null && !username.equals("") 
+                && email != null && !email.equals("") && password != null && !password.equals("") ) {
+
+            //check email & username !alreadyInDB      
+            TypedQuery<User> query;
+            query = (TypedQuery<User>) em.createQuery(
+                    "SELECT u FROM User u WHERE u.userName = :userName OR u.email = :email")
+                    .setParameter("userName", username)
+                    .setParameter("email", email);
+
+            if (query.getResultList().isEmpty()) {
+                //OK, email or username are not already in DB
+                return true;
+            }
+        }
+        //Submitted data not valid
+        return false;
+    }
 }
--- meteocal/src/main/java/com/meteocal/gui/RegistrationBean.java
+++ meteocal/src/main/java/com/meteocal/gui/RegistrationBean.java
@@ -11,71 +11,85 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 
-
 /**
  *
  * @author Jude
  */
 @ManagedBean
 @SessionScoped
-public class RegistrationBean implements Serializable{
-    
+public class RegistrationBean implements Serializable {
+
     @EJB
     RegistrationFacade rf;
-    
-    private String firstname, lastname, username, email,  password, emailagain;
 
+    private String firstname, lastname, username, email, password, emailagain;
+    
     public String getEmailagain() {
         return emailagain;
     }
+
     public void setEmailagain(String emailagain) {
         this.emailagain = emailagain;
-    }   
-    public String getFirstName() {
-       return firstname;
     }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
     public void setFirstName(String firstname) {
         this.firstname = firstname;
-    }    
+    }
+
     public String getLastName() {
         return lastname;
     }
+
     public void setLastName(String lastname) {
         this.lastname = lastname;
-    }    
+    }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public RegistrationBean() {
+
+    }
     
-    public String createAccount() {    //Jude - needs to check the provided data and create account
+    //TODO jdoc
+    /**
+     * 
+     * @return 
+     */
+    public String createAccount() {
         //check email and create user
-        if(this.email.trim().equals(this.emailagain.trim()) && rf.registerUser(firstname, lastname, username, email, password))
-                return "/home?faces-redirect=true";
-        
+        if ( //email.trim().equals( emailagain.trim()) && 
+                rf.registerUser(firstname, lastname, username, email, password)) {
+            return "/home?faces-redirect=true";
+        }
+
         //Submitted data not valid
         return "/registration?faces-redirect=true";
     }
-    
 
-
-    public RegistrationBean() {    //Jude - I don't know what this method is suppossed tp be doing.
-        
-    }
-    
-    
 }
