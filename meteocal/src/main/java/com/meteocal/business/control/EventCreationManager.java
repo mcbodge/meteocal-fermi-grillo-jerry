599a4268c5ac1967bec29f0a0bf95c1fe2c3215c
francesco.fermi@gmail.com <francesco.fermi@gmail.com@59154255-1539-2a9e-40eb-080a1e93b571>
2015-01-11 17:07:23 +0000

    overlap fixed

--- meteocal/src/main/java/com/meteocal/business/control/EventCreationManager.java
+++ meteocal/src/main/java/com/meteocal/business/control/EventCreationManager.java
@@ -7,6 +7,7 @@ package com.meteocal.business.control;
 
 import com.meteocal.business.entity.Event;
 import com.meteocal.business.entity.User;
+import com.meteocal.business.entity.Weather;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -15,19 +16,19 @@ import java.util.logging.Logger;
 import javax.inject.Inject;
 
 /**
- * FROM/TO - B:PersonalPage
- * TO - E:User, E:Event, C:EventManager
- * 
+ * FROM/TO - B:PersonalPage TO - E:User, E:Event, C:EventManager
+ *
  * @author Manuel
  */
 public class EventCreationManager {
-    
+
     @Inject
     EventManager ev_m;
 
     /**
      * Calculate when the event will be over
-     * @param dateStart 
+     *
+     * @param dateStart
      * @param duration
      * @return Date end date, when the event will be over.
      */
@@ -46,64 +47,67 @@ public class EventCreationManager {
 
         return cal.getTime(); // returns new date object, in the future
     }
-    
+
     /**
-     * NEW EVENT, GIVING THE LOCATION normal-language STRING
-     * Creates a new event, given the required parameters and returns the Event Obj.
-     * Requires valid creator, name, start and end.
-     * 
+     * NEW EVENT, GIVING THE LOCATION normal-language STRING Creates a new
+     * event, given the required parameters and returns the Event Obj. Requires
+     * valid creator, name, start and end.
+     *
      * @param creator the creator (User).
      * @param name the name of the event.
-     * @param start the start datetime of the event (w/ year, month, day, hour and minute).
-     * @param end the end datetime of the event (w/ year, month, day, hour and minute).
-     * @param location the location of the event, generated or given by the user, or null.
+     * @param start the start datetime of the event (w/ year, month, day, hour
+     * and minute).
+     * @param end the end datetime of the event (w/ year, month, day, hour and
+     * minute).
+     * @param location the location of the event, generated or given by the
+     * user, or null.
      * @param invited the list of the users invited to the event, or null.
      * @param p the privacy value 0 for public, 1 for private.
      * @param constraint the generated constraint value of the event, or null.
      * @param description the description of the event, or null.
      * @return null if no event is created.
      */
-    public Event newEvent(User creator, String name, Date start, Date end, String location, List<User> invited, boolean p, Integer constraint, String description, int numOverlappingEvents){
+    public Event newEvent(User creator, String name, Date start, Date end, String location, List<User> invited, boolean p, Integer constraint, String description, int numOverlappingEvents) {
         Event event_result = null;
         Logger.getLogger(EventCreationManager.class.getName()).log(Level.INFO, "---- START newEvent in EventCreationManager ---------------");
-        
-        if(name!= null && ev_m.verifyConsistency(creator, start, end, numOverlappingEvents)){
+
+        if (name != null && ev_m.verifyConsistency(creator, start, end, numOverlappingEvents)) {
             //consistency ok
             event_result = new Event(creator, name, location, start, end, p);
-           
-            if(description != null && !description.isEmpty()){
+
+            if (description != null && !description.isEmpty()) {
                 event_result.setDescription(description);
             }
-            
+
             //personal event if and only if the creator is the only attender.
-            if(invited == null || invited.isEmpty()) {
-                event_result.setPersonal(true); 
+            if (invited == null || invited.isEmpty()) {
+                event_result.setPersonal(true);
             } else {
                 event_result.setPersonal(false);
             }
-            
+
         }
-        
-        boolean param = (event_result != null);
-        
-        Logger.getLogger(EventCreationManager.class.getName()).log(Level.INFO, "------ result: {0}", param);
+
+        Logger.getLogger(EventCreationManager.class.getName()).log(Level.INFO, "------ result: {0}", event_result != null);
         Logger.getLogger(EventCreationManager.class.getName()).log(Level.INFO, "---- END newEvent in EventCreationManager ---------------");
-        
+
         return event_result;
     }
-    
-    /*
+
     /**
-     * NEW EVENT, GIVING THE LOCATION ID
-     * The name of the location is automatically generated***
-     * Creates a new event, given the required parameters and returns its id.
-     * Requires valid creator, name, start and end.
-     * 
+     * NEW EVENT, GIVING THE LOCATION ID The name of the location is
+     * automatically generated*** Creates a new event, given the required
+     * parameters and returns its id. Requires valid creator, name, start and
+     * end.
+     *
      * @param creator the creator's userId.
      * @param name the name of the event.
-     * @param start the start datetime of the event (w/ year, month, day, hour and minute).
-     * @param end the end datetime of the event (w/ year, month, day, hour and minute).
-     * @param geoname the generated location of the event (if it's null no Event is created).
+     * @param start the start datetime of the event (w/ year, month, day, hour
+     * and minute).
+     * @param end the end datetime of the event (w/ year, month, day, hour and
+     * minute).
+     * @param geoname the generated location of the event (if it's null no Event
+     * is created).
      * @param invited the list of the users invited to the event, or null.
      * @param p the privacy value 0 for public, 1 for private.
      * @param constraint the generated constraint value of the event, or null.
@@ -111,43 +115,31 @@ public class EventCreationManager {
      * @return the eventId of the created event. Null if no event is created.
      */
     /*
-    public Integer newEvent(User creator, String name, Date start, Date end, Integer geoname, List<User> invited, boolean p, Integer constraint, String description){
-        if(geoname != null && start != null && end != null && name!= null && EventManager.getInstance().verifyConsistency(creator, start, end)){
-            String location = em.createNamedQuery("Location.findByGeonameid",Location.class).setParameter("geonameid", geoname).getSingleResult().toString();
+    public Event newEvent(User creator, String name, Date start, Date end, Integer geoname, List<User> invited, boolean p, Integer constraint, String description, int numOverlappingEvents) {
+        
+        if (name != null && ev_m.verifyConsistency(creator, start, end, numOverlappingEvents)) {
+            //consistency ok
+        
+        //if (geoname != null && start != null && end != null && name != null && EventManager.getInstance().verifyConsistency(creator, start, end)) {
+            String location = em.createNamedQuery("Location.findByGeonameid", Location.class).setParameter("geonameid", geoname).getSingleResult().toString();
             Event event = new Event(creator, name, location, start, end, p);
-            
-            if(!description.isEmpty())
+
+            if (!description.isEmpty()) {
                 event.setDescription(description);
-            
+            }
+
             //personal event if and only if the creator is the only attender.
-            if( invited == null || invited.isEmpty()) {
+            if (invited == null || invited.isEmpty()) {
                 event.setPersonal(true);
             } else {
                 event.setPersonal(false);
             }
-            
-            //save in db
-            em.persist(event);
-            
-            //create weather constraint and bind it to the event
-            Weather weather;
-            weather = new Weather(event.getEventId(), geoname);
-            
-            if(constraint != null)
-                weather.setConstraint(constraint);
-            
-            //weather.setLocationCode(geoname);
-            
-            em.persist(weather);
-            
-            //send invitations
-            if(!invited.isEmpty())
-                EventManager.getInstance().sendInvitations(invited, event);
 
+            
             return event.getEventId();
         }
         //no consistency
         return null;
-    }
-    */
+    } */
+
 }
