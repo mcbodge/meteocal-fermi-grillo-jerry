30b25790c68b724256743c357bc826d2e5aca667
francesco.fermi@gmail.com <francesco.fermi@gmail.com@59154255-1539-2a9e-40eb-080a1e93b571>
2015-01-12 12:06:39 +0000

    notif page in progress

--- meteocal/src/main/java/com/meteocal/gui/NotificationsBean.java
+++ meteocal/src/main/java/com/meteocal/gui/NotificationsBean.java
@@ -6,19 +6,50 @@
 package com.meteocal.gui;
 
 import com.meteocal.business.boundary.NotificationsFacade;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author Manuel
  */
-public class NotificationsBean {
-    
+@Named
+@ViewScoped
+public class NotificationsBean implements Serializable {
+
     @EJB
     NotificationsFacade nf;
 
+    private List<String[]> string_list;
+
     public NotificationsBean() {
     }
-    
-    
+
+    //<editor-fold defaultstate="collapsed" desc="GETTERS AND SETTERS">
+    public List<String[]> getString_list() {
+        return string_list;
+    }
+
+    public void setString_list(List<String[]> string_list) {
+        this.string_list = string_list;
+    }
+    //</editor-fold>
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+    public void accept() {
+
+    }
+
+    public void decline() {
+
+    }
 }
