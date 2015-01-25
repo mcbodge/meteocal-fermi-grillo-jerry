f2aa3c8a58b1f93c2de7fa1d8ada737f2fb03bc1
manuel.grillo <manuel.grillo@aol.it>
2015-01-25 22:08:30 +0000

    BETA

--- meteocal/src/main/java/com/meteocal/gui/HomeBean.java
+++ meteocal/src/main/java/com/meteocal/gui/HomeBean.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.meteocal.gui;
 
 import com.meteocal.business.boundary.HomeFacade;
-import java.io.IOException;
 import javax.ejb.EJB;
 import java.io.Serializable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
