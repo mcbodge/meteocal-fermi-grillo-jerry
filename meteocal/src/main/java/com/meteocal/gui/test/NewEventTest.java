d314d5f26f8425225d109c90121c475874d1ac01
jerrymjude@gmail.com <jerrymjude@gmail.com@59154255-1539-2a9e-40eb-080a1e93b571>
2015-01-25 21:50:58 +0000

    All test classes are now in the test folder, you should delete the empty UserBean class

--- meteocal/src/main/java/com/meteocal/gui/test/NewEventTest.java
+++ meteocal/src/main/java/com/meteocal/gui/test/NewEventTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.meteocal.gui;
+package com.meteocal.gui.test;
 
 import com.meteocal.business.boundary.EventFacade;
 import com.meteocal.business.boundary.HomeFacade;
